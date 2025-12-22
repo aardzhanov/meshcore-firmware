9ac02044db9bc6603cfa0bd768c4066dc9f1cfe7
Anton Ardzhanov <aardzhanov@gmail.com>
2025-12-23 01:05:39 +0300

    meshadventure based esp2s3 + e22 radio work

--- variants/meshadventurer_esp32s3/variant.h
+++ variants/meshadventurer_esp32s3/variant.h
@@ -1,6 +1,6 @@
 // For OLED LCD
-#define I2C_SDA 21
-#define I2C_SCL 22
+#define I2C_SDA 17
+#define I2C_SCL 18
 
 // For GPS, 'undef's not needed
 #define GPS_TX_PIN 15
@@ -8,12 +8,12 @@
 #define PIN_GPS_EN 4
 #define GPS_POWER_TOGGLE // Moved definition from platformio.ini to here
 
-#define BUTTON_PIN 39  // The middle button GPIO on the T-Beam
-#define BATTERY_PIN 35 // A battery voltage measurement pin, voltage divider connected here to measure battery voltage
-#define ADC_CHANNEL ADC1_GPIO35_CHANNEL
-#define ADC_MULTIPLIER 2
-#define EXT_PWR_DETECT 4    // Pin to detect connected external power source for LILYGO® TTGO T-Energy T18 and other DIY boards
-#define EXT_NOTIFY_OUT 12   // Overridden default pin to use for Ext Notify Module (#975).
+#define BUTTON_PIN 0  // The middle button GPIO on the T-Beam
+//#define BATTERY_PIN 1 // A battery voltage measurement pin, voltage divider connected here to measure battery voltage
+//#define ADC_CHANNEL ADC1_GPIO1_CHANNEL
+//#define ADC_MULTIPLIER 2
+//#define EXT_PWR_DETECT 4    // Pin to detect connected external power source for LILYGO® TTGO T-Energy T18 and other DIY boards
+//#define EXT_NOTIFY_OUT 12   // Overridden default pin to use for Ext Notify Module (#975).
 #define LED_PIN 2           // add status LED (compatible with core-pcb and DIY targets)
 
 // Radio
@@ -22,20 +22,20 @@
 #define SX126X_MAX_POWER 22 // Outputting 22dBm from SX1262 results in ~30dBm E22-900M30S output (module only uses last stage of the YP2233W PA)
 #define SX126X_DIO3_TCXO_VOLTAGE 1.8 // E22 series TCXO reference voltage is 1.8V
 
-#define SX126X_CS 18    // EBYTE module's NSS pin
+#define SX126X_CS 4    // EBYTE module's NSS pin
 #define SX126X_SCK 5    // EBYTE module's SCK pin
-#define SX126X_MOSI 27  // EBYTE module's MOSI pin
-#define SX126X_MISO 19  // EBYTE module's MISO pin
-#define SX126X_RESET 23 // EBYTE module's NRST pin
-#define SX126X_BUSY 32  // EBYTE module's BUSY pin
-#define SX126X_DIO1 33  // EBYTE module's DIO1 pin
+#define SX126X_MOSI 10  // EBYTE module's MOSI pin
+#define SX126X_MISO 6  // EBYTE module's MISO pin
+#define SX126X_RESET 1 // EBYTE module's NRST pin
+#define SX126X_BUSY 40  // EBYTE module's BUSY pin
+#define SX126X_DIO1 47  // EBYTE module's DIO1 pin
 
 // The E22's TXEN pin is connected to MCU pin, E22's RXEN pin is connected to MCU pin (allows for ramping up PA before transmission
 // Don't define DIO2_AS_RF_SWITCH because we only use DIO2 or an MCU pin mutually exclusively to connect to E22's TXEN (to prevent
 // a short if they are both connected at the same time and there's a slight non-neglibible delay and/or voltage difference between
 // DIO2 and TXEN).
 #define SX126X_TXEN 13 // Schematic connects EBYTE module's TXEN pin to MCU
-#define SX126X_RXEN 14 // Schematic connects EBYTE module's RXEN pin to MCU
+#define SX126X_RXEN 12 // Schematic connects EBYTE module's RXEN pin to MCU
 
 #define LORA_CS SX126X_CS     // Compatibility with variant file configuration structure
 #define LORA_SCK SX126X_SCK   // Compatibility with variant file configuration structure
