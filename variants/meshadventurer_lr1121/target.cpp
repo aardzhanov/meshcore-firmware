ca675fc869be372f806214464fc9e3b516227e73
Anton Ardzhanov <aardzhanov@gmail.com>
2025-12-22 09:52:42 +0300

    esp32 + lr1121 work

--- variants/meshadventurer_lr1121/target.cpp
+++ variants/meshadventurer_lr1121/target.cpp
@@ -1,12 +1,11 @@
 #include <Arduino.h>
 #include "target.h"
-
+#include <helpers/radiolib/CustomLR1110Wrapper.h>
 #include <helpers/sensors/MicroNMEALocationProvider.h>
 
 MeshadventurerBoard board;
 
-static SPIClass spi;
-RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, spi);
+RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, SPI);
 WRAPPER_CLASS radio_driver(radio, board);
 
 ESP32RTCClock fallback_clock;
@@ -19,15 +18,64 @@ MASensorManager sensors = MASensorManager(nmea);
   MomentaryButton user_btn(PIN_USER_BTN, 1000, true);
 #endif
 
-bool radio_init() {
-  fallback_clock.begin();
-  rtc_clock.begin(Wire);
 
-#if defined(P_LORA_SCLK)
-  return radio.std_init(&spi);
-#else
-  return radio.std_init();
+#define RF_SWITCH_TABLE
+
+#ifdef RF_SWITCH_TABLE
+static const uint32_t rfswitch_dios[Module::RFSWITCH_MAX_PINS] = {
+  RADIOLIB_LR11X0_DIO5,
+  RADIOLIB_LR11X0_DIO6,
+  RADIOLIB_LR11X0_DIO7,
+  RADIOLIB_LR11X0_DIO8,
+  RADIOLIB_NC
+};
+
+static const Module::RfSwitchMode_t rfswitch_table[] = {
+  // mode                 DIO5  DIO6  DIO7  DIO8
+  { LR11x0::MODE_STBY,  {LOW,  LOW,  LOW,  LOW  }},
+  { LR11x0::MODE_RX,    {LOW,  LOW,  LOW,  LOW  }},
+  { LR11x0::MODE_TX,    {LOW,  LOW,  LOW,  HIGH }},
+  { LR11x0::MODE_TX_HP, {LOW,  LOW,  LOW,  HIGH }},
+  { LR11x0::MODE_TX_HF, {LOW,  HIGH, HIGH, LOW  }},
+  { LR11x0::MODE_GNSS,  {LOW,  LOW,  LOW,  LOW  }},
+  { LR11x0::MODE_WIFI,  {HIGH, LOW,  LOW,  LOW  }},
+  END_OF_MODE_TABLE,
+};
 #endif
+
+#ifndef LORA_CR
+  #define LORA_CR      5
+#endif
+
+
+bool radio_init() {
+  //rtc_clock.begin(Wire);
+
+#ifdef LR11X0_DIO3_TCXO_VOLTAGE
+  float tcxo = LR11X0_DIO3_TCXO_VOLTAGE;
+#else
+  float tcxo = 1.6f;
+#endif
+
+  SPI.begin(P_LORA_SCLK, P_LORA_MISO, P_LORA_MOSI);
+  int status = radio.begin(LORA_FREQ, LORA_BW, LORA_SF, LORA_CR, RADIOLIB_LR11X0_LORA_SYNC_WORD_PRIVATE, LORA_TX_POWER, 16, tcxo);
+  if (status != RADIOLIB_ERR_NONE) {
+    Serial.print("ERROR: radio init failed: ");
+    Serial.println(status);
+    return false;  // fail
+  }
+
+  radio.setCRC(2);
+  radio.explicitHeader();
+
+#ifdef RF_SWITCH_TABLE
+  radio.setRfSwitchTable(rfswitch_dios, rfswitch_table);
+#endif
+#ifdef RX_BOOSTED_GAIN
+  radio.setRxBoostedGainMode(RX_BOOSTED_GAIN);
+#endif
+
+  return true;  // success
 }
 
 uint32_t radio_get_rng_seed() {
