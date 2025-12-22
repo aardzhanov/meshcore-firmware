3671b8cca5cdafe19fdeb6ce8f6f30b1b105d636
Anton Ardzhanov <aardzhanov@gmail.com>
2025-12-22 09:55:19 +0300

    nrf52840 + lr1121 work

--- variants/promicro_lr1121/PromicroBoard.cpp
+++ variants/promicro_lr1121/PromicroBoard.cpp
@@ -11,21 +11,19 @@ void PromicroBoard::begin() {
     startup_reason = BD_STARTUP_NORMAL;
     btn_prev_state = HIGH;
   
-    pinMode(PIN_VBAT_READ, INPUT);
+/*     pinMode(PIN_VBAT_READ, INPUT);
 
     #ifdef BUTTON_PIN
       pinMode(BUTTON_PIN, INPUT_PULLUP);
-    #endif
+    #endif */
 
     #if defined(PIN_BOARD_SDA) && defined(PIN_BOARD_SCL)
       Wire.setPins(PIN_BOARD_SDA, PIN_BOARD_SCL);
     #endif
     
     Wire.begin();
-
-    pinMode(SX126X_POWER_EN, OUTPUT);
-    digitalWrite(SX126X_POWER_EN, HIGH);
-    delay(10);   // give sx1262 some time to power up
+    delay(10);
+    Serial.println("Starting meshcore");
 }
 
 static void connect_callback(uint16_t conn_handle) {
