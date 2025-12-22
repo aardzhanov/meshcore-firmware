4981be8030cd384fd60b1d542e400d46ffd973f7
Anton Ardzhanov <aardzhanov@gmail.com>
2025-12-22 09:55:19 +0300

    nrf52840 + lr1121 work

--- variants/promicro_lr1121/PromicroBoard.h
+++ variants/promicro_lr1121/PromicroBoard.h
@@ -3,18 +3,15 @@
 #include <MeshCore.h>
 #include <Arduino.h>
 
-#define P_LORA_NSS 13 //P1.13 45
-#define P_LORA_DIO_1 11 //P0.10 10
-#define P_LORA_RESET 10 //P0.09 9
-#define P_LORA_BUSY  16 //P0.29 29
-#define P_LORA_MISO  15 //P0.02 2
-#define P_LORA_SCLK  12 //P1.11 43
-#define P_LORA_MOSI  14 //P1.15 47
-#define SX126X_POWER_EN 21 //P0.13 13
-#define SX126X_RXEN 2 //P0.17
-#define SX126X_TXEN RADIOLIB_NC
-#define SX126X_DIO2_AS_RF_SWITCH  true
-#define SX126X_DIO3_TCXO_VOLTAGE (1.8f)
+#define P_LORA_MOSI 13 //P1.13 45
+#define P_LORA_RESET 11 //P0.10 10
+#define P_LORA_DIO_1 10 //P0.09 9
+#define P_LORA_SCLK  16 //P0.29 29
+#define P_LORA_NSS  15 //P0.02 2
+#define P_LORA_MISO  12 //P1.11 43
+#define P_LORA_BUSY  14 //P1.15 47
+#define LR11X0_DIO_AS_RF_SWITCH true
+#define LR11X0_DIO3_TCXO_VOLTAGE 3.0
 
 #define  PIN_VBAT_READ 17
 #define  ADC_MULTIPLIER   (1.815f) // dependent on voltage divider resistors. TODO: more accurate battery tracking
